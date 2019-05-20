761b2bf4afefb838b822152eb1bb979b9e008c09
Evgeni Ivanov <evgeniivanov.bg@gmail.com>
2019-05-20 23:46:53 +0100

    Solve Problem 03.01.SumOfNumbersN…M
    Solve Lecture 01 - Syntax, Functions And Statements/Lab/03.01.SumOfNumbersN…M/03.01.SumOfNumbersN…M.js problem

---
+++ 01 - Syntax, Functions And Statements/Lab/03.01.SumOfNumbersN…M/03.01.SumOfNumbersN…M.js
@@ -0,0 +1,23 @@
+/**
+ *
+ * @param {String} n
+ * @param {String} m
+ */
+
+function sumOfNumbersNToM(n, m) {
+    let number1 = Number(n);
+    let number2 = Number(m);
+    let sum = 0;
+
+    for (let i = number1; i <= number2; i++) {
+        sum += i;
+    }
+
+    return sum;
+
+    console.log(sum);
+}
+
+// Test
+sumOfNumbersNToM("1","5");
+sumOfNumbersNToM("1","100");
