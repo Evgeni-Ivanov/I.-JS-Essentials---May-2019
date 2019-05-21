3262bc2e2353352dec0a13fb0c686e4efd6fc478
Evgeni Ivanov <evgeniivanov.bg@gmail.com>
2019-05-21 22:39:24 +0100

    Solve Problem 04.01.LargestNumber
    Solve Lecture 01 - Syntax, Functions And Statements/Lab/04.01.LargestNumber/04.01.LargestNumber.js problem

---
+++ 01 - Syntax, Functions And Statements/Lab/04.01.LargestNumber/04.01.LargestNumber.js
@@ -0,0 +1,26 @@
+/**
+ *
+ * @param {Number} number1
+ * @param {Number} number2
+ * @param {Number} number3
+ */
+
+function largestNumber(number1, number2, number3) {
+    let result;
+
+    if (number1 > number2 && number1 > number3) {
+        result = number1;
+    }
+    else if (number2 > number1 && number2 > number3) {
+        result = number2;
+    }
+    else if (number3 > number1 && number3 > number2) {
+        result = number3;
+    }
+
+    console.log(`The largest number is ${result}.`);
+}
+
+// Test
+// largestNumber(100, 200, 300);
+// largestNumber(-100, -200, -300);
