2177dbff19749d3eb545452e3032fc7e46e6c75a
Evgeni Ivanov <evgeniivanov.bg@gmail.com>
2019-06-10 22:29:11 +0100

    Solve Problem 02.03.ChatRoom
    Solve Lecture 02 - Objects And DOM/Exercise/02.03.ChatRoom/02.03.ChatRoom.js problem

---
+++ 02 - Objects And DOM/Exercise/02.03.ChatRoom/02.03.ChatRoom.js
@@ -0,0 +1,22 @@
+function chatRoom() {
+    let chatInputArea = document.getElementById("chat_input");
+    let sendButton = document.getElementById("send");
+    let messageSection = document.getElementById("chat_messages");
+
+    sendButton.addEventListener("click", sendMessage);
+
+    function sendMessage() {
+
+        if(chatInputArea.value) {
+            let chatInputContent = chatInputArea.value;
+
+            let messageToSend = document.createElement("div");
+            messageToSend.setAttribute("class", "message my-message");
+            messageToSend.textContent = chatInputContent;
+
+            messageSection.appendChild(messageToSend);
+
+            chatInputArea.value = "";
+        }
+    }
+}
